9182a17c3f736f3b627bd5d5c9ed91c67c99462b
xyztWT <98135295+27182818284590@users.noreply.github.com>
2022-05-18 00:55:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,24 +13,24 @@
 
 var func = 2;
 function tick(){
-  var exp=window.calc.getState().expressions.list;
+  var exp=window.Calc.getState().expressions.list;
   exp.forEach(a=>{
     if(a.text){
         var a1;
-      if(a.text.substring(0,7)=="setLatex"){
+      if(a.text.substring(0,8)=="setLatex"){
         a1=exp[Number(a.text.substring(0,a.text.indexOf("\n")-1))-1];
         a1.latex=a.text.substring(a.text.indexOf("\n"),a.text.length)
         window.Calc.setExpression(a1)
-      } else if(a.text.substring(0,7)=="setLabel"){
+      } else if(a.text.substring(0,8)=="setLabel"){
         a1=exp[Number(a.text.substring(0,a.text.indexOf("\n")))-1];
         a1.label=a.text.substring(a.text.indexOf("\n")+1,a.text.length)
         window.Calc.setExpression(a1)
-      } else if(a.text.substring(0,9)=="labelLatex"){
+      } else if(a.text.substring(0,10)=="labelLatex"){
         a1=exp[Number(a.text.substring(0,a.text.indexOf(",")))-1];
         var a2=exp[Number(a.text.substring(a.text.indexOf(",")+1,a.text.indexOf("\n")))-1];
         a2.latex=a1.label
         window.Calc.setExpression(a2)
-      } else if(a.text.substring(0,7)=="function"){
+      } else if(a.text.substring(0,8)=="function"){
         if (func == 2){
           func = confirm("This graph uses javascript functions./nWould you like to enable them?/n(Note that this runs every millisecond.)")
         }
