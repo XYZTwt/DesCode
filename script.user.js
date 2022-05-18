5786f5df5e859831a436f88b5cb93e56f653a222
xyztWT <98135295+27182818284590@users.noreply.github.com>
2022-05-18 11:33:18 +0530

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -17,35 +17,35 @@ function tick(){
   var exp=window.Calc.getState().expressions.list;
   exp.forEach(a=>{
     if(a.text){
-      if(a.text.substring(0,7)=="disable"){
+      if(a.text.substring(0,9)=="j:disable"){
         enabled=0
-      } else if(a.text.substring(0,6)=="enable"){
+      } else if(a.text.substring(0,8)=="j:enable"){
         enabled=1
       }
       if (enabled){
         var a1;
-        if(a.text.substring(0,8)=="setLatex"){
-          a1=exp[Number(a.text.substring(8,a.text.indexOf("\n")))-1];
+        if(a.text.substring(0,10)=="j:setLatex"){
+          a1=exp[Number(a.text.substring(10,a.text.indexOf("\n")))-1];
           a1.latex=a.text.substring(a.text.indexOf("\n")+1,a.text.length)
           window.Calc.setExpression(a1)
-        } else if(a.text.substring(0,8)=="setLabel"){
-          a1=exp[Number(a.text.substring(8,a.text.indexOf("\n")))];
+        } else if(a.text.substring(0,10)=="j:setLabel"){
+          a1=exp[Number(a.text.substring(10,a.text.indexOf("\n")))];
           a1.label=a.text.substring(a.text.indexOf("\n")+1,a.text.length)
           window.Calc.setExpression(a1)
-        } else if(a.text.substring(0,10)=="labelLatex"){
-            a1=exp[Number(a.text.substring(10,a.text.indexOf(",")))-1];
-            var a2=exp[Number(a.text.substring(a.text.indexOf(",")+1,a.text.indexOf("\n")))-1];
+        } else if(a.text.substring(0,12)=="j:labelLatex"){
+          a1=exp[Number(a.text.substring(12,a.text.indexOf(",")))-1];
+          var a2=exp[Number(a.text.substring(a.text.indexOf(",")+1,a.text.indexOf("\n")))-1];
           if (a1.label){
             if (a1.editableLabelMode=="MATH"){
               a2.latex=a1.label.substring(1,a1.label.length-1)
-            } else{
+            } else {
               a2.latex=a1.label
             }
           } else{
             a2.latex = ""
           }
           window.Calc.setExpression(a2)
-        } else if(a.text.substring(0,8)=="function"){
+        } else if(a.text.substring(0,10)=="j:function"){
           if (func == 2){
             func = confirm("This graph uses javascript functions. Would you like to enable them? (Note that this runs every millisecond.)")
           }
