9614f0c1631d75afaa83526bad66be5ac3721a3f
xyztWT <98135295+27182818284590@users.noreply.github.com>
2022-05-18 12:46:08 +0530

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -25,24 +25,24 @@ function tick(){
       if (enabled){
         var a1;
         if(a.text.substring(0,10)=="j:setLatex"){
-          a1=exp[Number(a.text.substring(10,a.text.indexOf("\n")))-1];
+          a1=exp[Number(a.text.substring(10,a.text.indexOf("\n")))-1]
           a1.latex=a.text.substring(a.text.indexOf("\n")+1,a.text.length)
           window.Calc.setExpression(a1)
         } else if(a.text.substring(0,10)=="j:setLabel"){
-          a1=exp[Number(a.text.substring(10,a.text.indexOf("\n")))];
+          a1=exp[Number(a.text.substring(10,a.text.indexOf("\n")))]
           a1.label=a.text.substring(a.text.indexOf("\n")+1,a.text.length)
           window.Calc.setExpression(a1)
         } else if(a.text.substring(0,12)=="j:labelLatex"){
-          a1=exp[Number(a.text.substring(12,a.text.indexOf(",")))-1];
+          a1=exp[Number(a.text.substring(12,a.text.indexOf(",")))-1]
           var a2=exp[Number(a.text.substring(a.text.indexOf(",")+1,a.text.indexOf("\n")))-1];
           if (a1.label){
             if (a1.editableLabelMode=="MATH"){
-              a2.latex=a1.label.substring(1,a1.label.length-1)
+              a2.latex=a.text.substring(a.text.indexOf("\n")+1,a.text.lastIndexOf("\n"))+a1.label.substring(1,a1.label.length-1)+a.text.substring(a.text.lastIndexOf("\n")+1,a.text.length)
             } else {
-              a2.latex=a1.label
+              a2.latex=a.text.substring(a.text.indexOf("\n")+1,a.text.lastIndexOf("\n"))+a1.label+a.text.substring(a.text.lastIndexOf("\n")+1,a.text.length)
             }
           } else{
-            a2.latex = ""
+            a2.latex = a.text.substring(a.text.indexOf("\n")+1,a.text.lastIndexOf("\n"))+a.text.substring(a.text.lastIndexOf("\n")+1,a.text.length)
           }
           window.Calc.setExpression(a2)
         } else if(a.text.substring(0,10)=="j:function"){
@@ -50,7 +50,7 @@ function tick(){
             func = confirm("This graph uses javascript functions. Would you like to enable them? (Note that this runs every centisecond.)")
           }
           if (func){
-            a1 = Function(a.text.substring(a.text.indexOf("\n")+1,a.text.length));
+            a1 = Function(a.text.substring(a.text.indexOf("\n")+1,a.text.length))
             a1()
           }
         }
