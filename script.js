db9730b002dd2f447ed25dda6b16cbf0934433f3
xyztWT <98135295+27182818284590@users.noreply.github.com>
2022-05-18 10:25:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,39 +12,47 @@
 // ==/UserScript==
 
 var func = 2;
+var enabled = 1;
 function tick(){
   var exp=window.Calc.getState().expressions.list;
   exp.forEach(a=>{
     if(a.text){
+      if(a.text.substring(0,7)=="disable"){
+        enabled=0
+      } else if(a.text.substring(0,6)=="enable"){
+        enabled=1
+      }
+      if (enabled){
         var a1;
-      if(a.text.substring(0,8)=="setLatex"){
-        a1=exp[Number(a.text.substring(9,a.text.indexOf("\n")-1))-1];
-        a1.latex=a.text.substring(a.text.indexOf("\n"),a.text.length)
-        window.Calc.setExpression(a1)
-      } else if(a.text.substring(0,8)=="setLabel"){
-        a1=exp[Number(a.text.substring(8,a.text.indexOf("\n")))-1];
-        a1.label=a.text.substring(a.text.indexOf("\n")+1,a.text.length)
-        window.Calc.setExpression(a1)
-      } else if(a.text.substring(0,10)=="labelLatex"){
-        a1=exp[Number(a.text.substring(10,a.text.indexOf(",")))-1];
-        var a2=exp[Number(a.text.substring(a.text.indexOf(",")+1,a.text.indexOf("\n")))-1];
-        if (a1.label){
-          if (a1.editableLabelMode=="MATH"){
+        if(a.text.substring(0,8)=="setLatex"){
+          a1=exp[Number(a.text.substring(9,a.text.indexOf("\n")-1))-1];
+          a1.latex=a.text.substring(a.text.indexOf("\n"),a.text.length)
+          window.Calc.setExpression(a1)
+        } else if(a.text.substring(0,8)=="setLabel"){
+          a1=exp[Number(a.text.substring(8,a.text.indexOf("\n")))-1];
+          a1.label=a.text.substring(a.text.indexOf("\n")+1,a.text.length)
+          window.Calc.setExpression(a1)
+        } else if(a.text.substring(0,10)=="labelLatex"){
+          a1=exp[Number(a.text.substring(10,a.text.indexOf(",")))-1];
+          var a2=exp[Number(a.text.substring(a.text.indexOf(",")+1,a.text.indexOf("\n")))-1];
+          if (a1.label){
+            if (a1.editableLabelMode=="MATH"){
               a2.latex=a1.label.substring(1,a1.label.length-1)
-          } else {
-                a2.latex=a1.label
-          }
-        } else{
+            } else {
+              a2.latex=a1.label
+            }
+          } else{
             a2.latex = ""
-        }
-        window.Calc.setExpression(a2)
-      } else if(a.text.substring(0,8)=="function"){
-        if (func == 2){
-          func = confirm("This graph uses javascript functions. Would you like to enable them? (Note that this runs every millisecond.)")
-        }
-        if (func){
-          a1 = Function(a.text.substring(a.text.indexOf("\n")+1,a.text.length));
-          a1()
+          }
+          window.Calc.setExpression(a2)
+        } else if(a.text.substring(0,8)=="function"){
+          if (func == 2){
+            func = confirm("This graph uses javascript functions. Would you like to enable them? (Note that this runs every millisecond.)")
+          }
+          if (func){
+            a1 = Function(a.text.substring(a.text.indexOf("\n")+1,a.text.length));
+            a1()
+          }
         }
       }
     }
