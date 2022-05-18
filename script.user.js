75944a7598988d5272fa21d5cc22e4d75dfedc6b
xyztWT <98135295+27182818284590@users.noreply.github.com>
2022-05-18 11:21:39 +0530

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -7,8 +7,8 @@
 // @run-at      document-start
 // @author      xyztWT
 // @description Note Coding!
-// @downloadURL https://github.com/27182818284590/DesCode/raw/main/script.js
-// @updateURL   https://github.com/27182818284590/DesCode/raw/main/script.js
+// @downloadURL https://github.com/27182818284590/DesCode/raw/script.js
+// @updateURL   https://github.com/27182818284590/DesCode/raw/script.js
 // ==/UserScript==
 
 var func = 2;
@@ -25,20 +25,20 @@ function tick(){
       if (enabled){
         var a1;
         if(a.text.substring(0,8)=="setLatex"){
-          a1=exp[Number(a.text.substring(9,a.text.indexOf("\n")-1))-1];
-          a1.latex=a.text.substring(a.text.indexOf("\n"),a.text.length)
+          a1=exp[Number(a.text.substring(8,a.text.indexOf("\n")))-1];
+          a1.latex=a.text.substring(a.text.indexOf("\n")+1,a.text.length)
           window.Calc.setExpression(a1)
         } else if(a.text.substring(0,8)=="setLabel"){
-          a1=exp[Number(a.text.substring(8,a.text.indexOf("\n")))-1];
+          a1=exp[Number(a.text.substring(8,a.text.indexOf("\n")))];
           a1.label=a.text.substring(a.text.indexOf("\n")+1,a.text.length)
           window.Calc.setExpression(a1)
         } else if(a.text.substring(0,10)=="labelLatex"){
-          a1=exp[Number(a.text.substring(10,a.text.indexOf(",")))-1];
-          var a2=exp[Number(a.text.substring(a.text.indexOf(",")+1,a.text.indexOf("\n")))-1];
+            a1=exp[Number(a.text.substring(10,a.text.indexOf(",")))-1];
+            var a2=exp[Number(a.text.substring(a.text.indexOf(",")+1,a.text.indexOf("\n")))-1];
           if (a1.label){
             if (a1.editableLabelMode=="MATH"){
               a2.latex=a1.label.substring(1,a1.label.length-1)
-            } else {
+            } else{
               a2.latex=a1.label
             }
           } else{
